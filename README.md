d681cd32e05f05df7f146422992992572b464774
mohap <mohapmobile@gmail.com>
2018-07-03 19:02:05 +0200

    feat : added movement and finished

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+# Frogger Arcade Game
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## discription
+Frogger Arcade is a variation of the classic Frogger arcade game.
+
+
+## Installation
+
+### To run the application:
+
+1. Download the GitHub zip file or clone the repository onto your local workstation:
+	* git clone [https://github.com/alexsales/frontend-nanodegree-frogger-arcade-game.git/]()
+2. open `index.html` file in google chrome.
+
+### how to play
+Users move the hero character by using the main up/right/left/down arrow keys on their keyboard
+the player wins when he reaches the top row.
--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x , y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.speed = speed;
+    this.y = y;
+    this.x = x;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,7 +16,50 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    if(this.x >= 505){
+      this.x = 0;
+    }
+
+
+    function  checkCollision(anEnemy) {
+        // check for collision between enemy and player
+        if (
+            player.y + 131 >= anEnemy.y + 90
+            && player.x + 25 <= anEnemy.x + 88
+            && player.y + 73 <= anEnemy.y + 135
+            && player.x + 76 >= anEnemy.x + 11) {
+            console.log('collided');
+            player.x = 202.5;
+            player.y = 383;
+        }
+
+        // check for player reaching top of canvas and winning the game
+        // if player wins, add 1 to the score and level
+        // pass score as an argument to the increaseDifficulty function
+        if (player.y + 63 <= 0) {
+            player.x = 202.5;
+            player.y = 383;
+            console.log('you made it!');
+        }
+
+        // check if player runs into left, bottom, or right canvas walls
+        // prevent player from moving beyond canvas wall boundaries
+        if (player.y > 383 ) {
+            player.y = 383;
+        }
+        if (player.x > 402.5) {
+            player.x = 402.5;
+        }
+        if (player.x < 2.5) {
+            player.x = 2.5;
+        }
+    };
+
+    checkCollision(this);
 };
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -26,16 +71,54 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 
+let Player = function (x , y, speed){
+  this.y = y;
+  this.x = x;
+  this.speed = speed;
+  this.sprite = 'images/char-boy.png';
+  this.render = ()=> {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+}
+
+
+Player.prototype.update = ()=>{
+   // leave it for now
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        player.x -= player.speed;
+    }
+    if (keyPress == 'up') {
+        player.y -= player.speed - 20;
+    }
+    if (keyPress == 'right') {
+        player.x += player.speed;
+    }
+    if (keyPress == 'down') {
+        player.y += player.speed - 20;
+    }
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+let player = new Player(202.5, 383, 50);
+let enemy1 = new Enemy(0, Math.random() * 184 + 30, Math.random() * 256);
+let enemy2 = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+let enemy3 = new Enemy(0, Math.random() * 184 + 80, Math.random() * 256);
 
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
